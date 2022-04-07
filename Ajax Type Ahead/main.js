5d82b02a05937d2f2a36d18bc16aa55b19763809
unimeang <aodgkfud4@gmail.com>
2022-04-07 16:58:36 +0900

    ✨ [add] : json data 연결

---
+++ Ajax Type Ahead/main.js
@@ -0,0 +1,6 @@
+const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+const cities = [];
+fetch(url)
+    .then(res => res.json())
+    .then(data => cities.push(...data));
