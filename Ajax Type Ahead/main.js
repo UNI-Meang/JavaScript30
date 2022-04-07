fb6c71c013405f9006ba19daec360ef112d2c508
unimeang <aodgkfud4@gmail.com>
2022-04-07 17:05:38 +0900

    ✨  [feat] : math 결과가 일치하는 것 filtering

--- Ajax Type Ahead/main.js
+++ Ajax Type Ahead/main.js
@@ -4,3 +4,12 @@ const cities = [];
 fetch(url)
     .then(res => res.json())
     .then(data => cities.push(...data));
+
+// filter로 math 결과가 city or state 일 경우
+function findMatchCities(wordToMatch, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return place.city.match(regex) || place.state.match(regex)
+    });
+}
+
