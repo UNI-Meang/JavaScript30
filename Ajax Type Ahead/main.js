6037b6ebf4e8de0538da4e11369d2e4812c5b339
unimeang <aodgkfud4@gmail.com>
2022-04-07 17:16:46 +0900

    ✨  [feat] : match 결과 값 display 기능 추가

--- Ajax Type Ahead/main.js
+++ Ajax Type Ahead/main.js
@@ -1,5 +1,9 @@
 const url = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
+const searchInput = document.querySelector('.search'); 
+const suggestions = document.querySelector('.suggestions');
+
+
 const cities = [];
 fetch(url)
     .then(res => res.json())
@@ -13,3 +17,22 @@ function findMatchCities(wordToMatch, cities) {
     });
 }
 
+// filter 결과값 display 해주기
+function displayMatches() {
+    const matchArray = findMatchCities(this.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="h1">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="h1">${this.value}</span>`);
+        return `
+        <li>
+            <span class="name">&{cityName}, ${stateName}</span>
+            <span class="population">${numberwithCommas(place.population)}</span>
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
