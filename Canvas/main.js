35777fbaf2a7aba85da597f1b9dc2966b43a1afb
unimeang <aodgkfud4@gmail.com>
2022-04-11 15:58:54 +0900

    ✅  [add] : drawing 기본값 설정

--- Canvas/main.js
+++ Canvas/main.js
@@ -1,5 +1,20 @@
 const canvas = document.querySelector('#draw');
 
 const ctx = canvas.getContext('2d');
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+ctx.strokeStyle = '#bada55';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = '100';
+ctx.globalCompositeOperation = 'multiply';
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 120;
+let direction = true;
+
+
