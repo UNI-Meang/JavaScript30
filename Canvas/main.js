2c46dbe00af15d8def86868bf4184f360179c874
unimeang <aodgkfud4@gmail.com>
2022-04-11 16:20:30 +0900

    ✅  [feat] : strokeStyle random color로 변경

--- Canvas/main.js
+++ Canvas/main.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-ctx.strokeStyle = '#bada55';
+ctx.strokeStyle = '#33B6FF';
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = '100';
@@ -14,32 +14,38 @@ ctx.globalCompositeOperation = 'multiply';
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
-let hue = 120;
 let direction = true;
+
+function randomColor() {
+    return Math.floor(Math.random() * 361);
+}
+
+let hue = randomColor();
+
 
 const draw = ({ offsetX, offsetY}) => {
     if (!isDrawing) return;
 
-    ctx.strokeStyle = `hsl(${hue}), 100%, 50%`;
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
     ctx.beginPath();
     ctx.moveTo(lastX, lastY);
     ctx.lineTo(offsetX, offsetY);
     ctx.stroke();
 
     [lastX, lastY] = [offsetX, offsetY];
-    hue >= 360 ? (hue = 0) : hue++;
+    hue >= 360 ? hue = 0 : hue++;
 
     (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) && (direction = !direction);
     direction ? ctx.lineWidth++ : ctx.lineWidth--;
 
 };
 
+canvas.addEventListener('mousemove', draw);
 canvas.addEventListener('mousedown', (e) => {
     isDrawing = true;
     [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', () => (isDrawing = false));
-canvas.addEventListener('mouseout', () => (isDrawing = false));
+canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mouseout', () => isDrawing = false);
 
