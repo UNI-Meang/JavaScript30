c3b2a56cbac945c8c9a4a67d02adf943077f6d6f
unimeang <aodgkfud4@gmail.com>
2022-04-13 20:28:05 +0900

    💄  [add] : console창 꾸미기 5가지

--- Dev tools domination/main.js
+++ Dev tools domination/main.js
@@ -3,3 +3,21 @@ function makeGreen() {
     p.style.color = '#BABA55';
     p.style.fontSize = '55px';
 }
+
+// Regular
+console.log('hi!');
+
+// Interpolated : %s
+console.log('Hi! I am %s Maeng!','😃');
+
+// Styled : %C
+console.log('%c I am a styled text', 'font-size: 30px; border: 5px solid blue; padding: 5px;')
+
+// warning 
+console.warn('OH! NO!');
+
+// Error 
+console.error('ERROR!');
+
+// Info
+console.info('😎 This is an info!');
