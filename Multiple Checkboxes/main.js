c8e347de097b3dd16b8ecefea2d5d000f11f3112
unimeang <aodgkfud4@gmail.com>
2022-04-15 23:29:04 +0900

    ✅  [feat] : shiftkey event loop 확인

--- Multiple Checkboxes/main.js
+++ Multiple Checkboxes/main.js
@@ -2,8 +2,13 @@ const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
 let lastChecked;
 
-function handledCheck(){
-    
+function handleCheck(e){
+    let inBetween = false;
+    if (e.shiftKey) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+        })
+    }
 }
 
 checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
