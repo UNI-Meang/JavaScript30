26cd91802fb1831b8ed18b90e5a6ecdc64c0c890
unimeang <aodgkfud4@gmail.com>
2022-04-21 19:09:00 +0900

    ✅  [feat] : keyup, secret code 일치 시 unicorn image 등장

--- Key Sequence Detection/main.js
+++ Key Sequence Detection/main.js
@@ -12,9 +12,22 @@
             'a',
         ];
 
+        const equalArray = (arr1, arr2) => {
+            if (arr1.length != arr2.length) return false;
+            for (let i = 0; i < arr1.length; i++) {
+                if (arr1[i] !== arr2[i]) return false;
+            }
+            return true;
+        };
 
         window.addEventListener('keyup', (e) => {
             console.log(e.key);
             pressed.push(e.key);
             console.log(pressed);
+
+            pressed.splice(-secretCode.length - 1,
+                pressed.length - secretCode.length
+            );
+
+            equalArray(pressed, secretCode) && cornify_add();
         });
