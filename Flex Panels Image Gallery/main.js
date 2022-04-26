ccc60df5b630587750ce2a2840ee5770e9ccfa8a
unimeang <aodgkfud4@gmail.com>
2022-04-26 09:07:32 +0900

    📝  [add] : Slide in on Scroll file 추가

--- Flex Panels Image Gallery/main.js
+++ Flex Panels Image Gallery/main.js
@@ -16,4 +16,4 @@ function toggleActive(e) {
 //     }
 // }
 panels.forEach(panel => panel.addEventListener('click', toggleOpen));
-panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));∫
