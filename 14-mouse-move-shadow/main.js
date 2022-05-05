889614436d57a139a4b9ad38aa74a950b94479bf
unimeang <aodgkfud4@gmail.com>
2022-05-05 22:00:38 +0900

    💄 [style] : text 움직임에 따른 shadow style 지정

--- 14-mouse-move-shadow/main.js
+++ 14-mouse-move-shadow/main.js
@@ -14,7 +14,13 @@ function shadow(e) {
     const xWalk = Math.round((x / width * walk) - (walk / 2));
     const yWalk = Math.round((y / height * walk) - (walk / 2));
 
-    
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk} 0 rgba(255,0,255,0.7),
+    ${xWalk * -1}px ${yWalk} 0 rgba(0,255,255,0.7),
+    ${yWalk}px ${xWalk * -1} 0 rgba(0,255,0,0.7),
+    ${yWalk * -1}px ${xWalk} 0 rgba(0,0,255,0.7),
+
+    `
 }
 
 hero.addEventListener('mousemove', shadow);
