8b41efe9a29b1457f720fde57c94ef1e822ddccf
unimeang <aodgkfud4@gmail.com>
2022-05-07 21:53:39 +0900

    🚑  [fix] : fix bug

--- 14-mouse-move-shadow/main.js
+++ 14-mouse-move-shadow/main.js
@@ -1,10 +1,16 @@
 const hero = document.querySelector('.hero');
-const text = document.querySelector('h1');
-const walk = 500;
+const text = hero.querySelector('h1');
+const walk = 300; // 500px
 
 function shadow(e) {
-    const { offsetWidth: width, offsetHeight: height } = hero;
-    let { offsetX: x, offsetY: y } = e;
+    const {
+        offsetWidth: width,
+        offsetHeight: height
+    } = hero;
+    let {
+        offsetX: x,
+        offsetY: y
+    } = e;
 
     if (this !== e.target) {
         x = x + e.target.offsetLeft;
@@ -15,12 +21,12 @@ function shadow(e) {
     const yWalk = Math.round((y / height * walk) - (walk / 2));
 
     text.style.textShadow = `
-    ${xWalk}px ${yWalk} 0 rgba(255,0,255,0.7),
-    ${xWalk * -1}px ${yWalk} 0 rgba(0,255,255,0.7),
-    ${yWalk}px ${xWalk * -1} 0 rgba(0,255,0,0.7),
-    ${yWalk * -1}px ${xWalk} 0 rgba(0,0,255,0.7),
+    ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk * -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
 
-    `
 }
 
 hero.addEventListener('mousemove', shadow);
